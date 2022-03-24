2cf86517d278106ed25e51cc5d8263951202a1ac
Valeriya Abdrakhmanova <valeriya.avt@gmail.com>
2022-03-24 08:50:42 +0300

    update BookDao

--- src/main/java/ru/msu/cmc/webapp/Dao/BookDao.java
+++ src/main/java/ru/msu/cmc/webapp/Dao/BookDao.java
@@ -8,12 +8,12 @@ public interface BookDao {
     void createBook(Book book);
     void updateBook(Book book);
     void deleteBook(Book book);
-    Book readBookByID(int id);
+    Book getBookByID(int id);
     List<Book> getBooksByTitle(String title);
     List<Book> getBooksByAuthor(String author);
     List<Book> getBooksByGenre(String genre);
     List<Book> getBooksByPublishingHouse(String publishing_house);
-    List<Book> getBooksByPublishingYear(String publishing_year);
+    List<Book> getBooksByPublishingYear(int publishing_year);
     double getBookPrice(Book book);
     int getBookAmount(Book book);
     List<Book> getAllBooks();
