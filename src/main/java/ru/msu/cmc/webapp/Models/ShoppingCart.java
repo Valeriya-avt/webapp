e0cc637bebc462f0c308e4080001d961c549c07b
Valeriya Abdrakhmanova <valeriya.avt@gmail.com>
2022-03-28 19:49:15 +0300

    update ShoppingCart class

--- src/main/java/ru/msu/cmc/webapp/Models/ShoppingCart.java
+++ src/main/java/ru/msu/cmc/webapp/Models/ShoppingCart.java
@@ -35,4 +35,18 @@ public class ShoppingCart {
 
     @Column(nullable = false, name = "amount")
     private int amount;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ShoppingCart shoppingCart = (ShoppingCart) o;
+        return this.shopping_cart_id == shoppingCart.shopping_cart_id &&
+                this.order_id.getOrder_id() == (shoppingCart.order_id.getOrder_id()) &&
+                this.book_id.getBook_id() == (shoppingCart.book_id.getBook_id()) &&
+                Integer.compare(shoppingCart.amount, this.amount) == 0 &&
+                Double.compare(shoppingCart.price, this.price) == 0;
+    }
 }
